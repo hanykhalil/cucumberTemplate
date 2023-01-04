cfc736d65d5b57d428cbcd13e55e34ff08c33c1a
Hany Khalil <hany.khalil@expleogroup.com>
2023-01-04 15:21:23 +0100

    update to solve

--- src/main/java/com/templates/cucumberTemplate/BaseTest.java
+++ src/main/java/com/templates/cucumberTemplate/BaseTest.java
@@ -18,11 +18,11 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class BaseTest {
 
 	public static WebDriver driver;
-//	public static final String USERNAME = "khalilhany_KLrQIy";
-//    public static final String AUTOMATE_KEY = "ckCHbxQXw1dXpFeLpzBC";
-//    public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
+	public static final String USERNAME = "khalilhany_KLrQIy";
+    public static final String AUTOMATE_KEY = "ckCHbxQXw1dXpFeLpzBC";
+    public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
 	
-	public void LunchTheBrowser() throws MalformedURLException {
+	public void LunchTheBrowser() throws MalformedURLException  {
 //		DesiredCapabilities capabilities = new DesiredCapabilities();
 //		capabilities.setBrowserName("chrome");
 //		capabilities.setPlatform(Platform.WIN10);
