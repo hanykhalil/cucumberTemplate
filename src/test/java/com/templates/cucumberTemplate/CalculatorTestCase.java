bcefaa124de3840517d9b22378bfae2bcfe5dabc
Hany Khalil <hany.khalil@expleogroup.com>
2023-01-27 15:33:21 +0100

    update-9

--- src/test/java/com/templates/cucumberTemplate/CalculatorTestCase.java
+++ src/test/java/com/templates/cucumberTemplate/CalculatorTestCase.java
@@ -5,13 +5,13 @@ import org.testng.annotations.Test;
 
 public class CalculatorTestCase {
 	CalculatorApp calculator= new CalculatorApp();
-	@Test
+	
 	public void addTwoPositiveNumbers()
 	{
 		int result= calculator.AddTwoNumbers(2, 7);
 		Assert.assertEquals(9, result);
 	}
-	@Test
+	
 	public void addTwoNegativeNumbers()
 	{
 		int result= calculator.AddTwoNumbers(-2, -7);
